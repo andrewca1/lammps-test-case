5dcd9448023ecfa4e3a85f415891718fc2abfbf0
Quang T. Ha <qth20@bu.edu>
2017-11-01 13:05:49 -0400

    Working reaction

--- mix_conc_diffusion/run.sh
+++ mix_conc_diffusion/run.sh
@@ -3,7 +3,10 @@
 # Change directory to exe
 exedir=/home/qth20/Documents/lammps-sph/lammps/src
 
+dname=results
+mkdir -p ${dname}
+
 lmp=${exedir}/lmp_serial
 
-${lmp} -in sph_mix_conc_diffusion_2d.lmp 
+${lmp} -in sph_mix_conc_diffusion_2d.lmp -var dname ${dname}
 
