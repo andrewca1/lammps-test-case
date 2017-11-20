b6c6d9d2b2d5f62aa7f92512bddab670fa779941
Quang Ha <qth20@bu.edu>
2017-11-20 00:14:46 -0500

    Playing around with passing message

--- concentration_diffusion/run.sh
+++ concentration_diffusion/run.sh
@@ -3,6 +3,10 @@
 # Change directory to exe
 exedir=/home/qth20/Documents/lammps-sph/lammps/src
 
+# lmp=${exedir}/lmp_mpi
+
+# mpirun -np 4 ${lmp} -in sph_conc_diffusion_2d.lmp 
+
 lmp=${exedir}/lmp_serial
 
 ${lmp} -in sph_conc_diffusion_2d.lmp 
