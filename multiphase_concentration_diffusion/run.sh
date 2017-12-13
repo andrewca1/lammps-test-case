fa4fbfb50a38bb37993eb3fb6bf9c7df2d82ceb9
Quang T. Ha <qth20@bu.edu>
2017-12-13 17:03:46 -0500

    Add multiphase case

---
+++ multiphase_concentration_diffusion/run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Change directory to exe
+exedir=/home/qth20/Documents/lammps-sph/lammps/src
+
+# lmp=${exedir}/lmp_mpi
+
+# mpirun --mca orte_base_help_aggregate 0 -np 4 ${lmp} -in sph_conc_diffusion_2d.lmp 
+
+lmp=${exedir}/lmp_serial
+
+${lmp} -in sph_multiphase_conc_diffusion_2d.lmp 
+
