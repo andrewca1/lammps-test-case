2c9867a6cf61ea78a4ea91d6bb72bf92fcb412e7
Quang T. Ha <qth20@bu.edu>
2017-12-19 17:06:31 -0500

    Working concentration around cylinder now

--- concentration_around_cylinder/run.sh
+++ concentration_around_cylinder/run.sh
@@ -4,8 +4,10 @@
 exedir=/home/qth20/Documents/lammps-sph/lammps/src
 
 dname=data-wall
-lmp=${exedir}/lmp_mpi
 mkdir -p ${dname}
 
-${lmp} -in flow.lmp -var dname ${dname}
+# lmp=${exedir}/lmp_mpi
+# mpirun -np 4 ${lmp} -in flow.lmp -var dname ${dname}
 
+lmp=${exedir}/lmp_serial
+${lmp} -in flow.lmp -var dname ${dname}
