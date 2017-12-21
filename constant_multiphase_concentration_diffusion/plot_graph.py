0d5a36e4a5feaa0b9c2216f8d1eb6d1ca8dedb06
Quang T. Ha <qth20@bu.edu>
2017-12-21 17:22:15 -0500

    Add constant fix

---
+++ constant_multiphase_concentration_diffusion/plot_graph.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy import special
+import matplotlib.pyplot as plt
+import itertools
+
+# parameters
+alpha = 1.0e-4
+K = 1.0e-4
+el    = 2.0
+er    = 1.0
+
+# domain
+Lx = 1.0
+# splitting point
+xc = 0.5*Lx
+
+# Time series
+T = [100]
+
+# plot
+plt.figure(figsize=(16, 9))
+ax = plt.gca()
+
+# marker tools
+marker = itertools.cycle(('o', 'v', '^', '<', '>', 's', '8', 'p'))
+
+
+for time in T:
+    # analytical solution
+    x = np.linspace(0.0, 1.0, 100)
+    e_analytic = 0.5*(el+er) + 0.5*(er - el)*special.erf((x-xc)/np.sqrt(4*alpha*time))
+    e_analytic = np.exp(-K*time)*e_analytic
+    
+    # read data from output
+    data = np.loadtxt("dump_{}.last".format(time), skiprows=9)
+
+    color = next(ax._get_lines.prop_cycler)['color']
+    plt.plot(data[:,2], data[:,5], linestyle='', markeredgecolor='none', marker=next(marker), color=color, label="sph-{}".format(time))
+    plt.plot(x, e_analytic, label="analytical-{}".format(time), linestyle='-', color=color)
+    
+plt.ylabel("Temperature")
+plt.ylim(0.8, 2.2)
+plt.xlim(0.0, Lx)
+plt.xlabel("x")
+plt.legend()
+plt.savefig("results.png")
+
