f9b611b8b4c893f9bd84e18718ac64ff9ef576f6
Quang T. Ha <qth20@bu.edu>
2018-01-02 17:32:44 -0500

    Playing with precipitation in flow

---
+++ multiphase_flow_precipitation/run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Change directory to exe
+exedir=/home/qth20/Documents/lammps-sph/lammps/src
+
+# lmp=${exedir}/lmp_mpi
+
+# mpirun -np 4 ${lmp} -in sph_flow_conc_diffusion_2d.lmp 
+dname=data
+lmp=${exedir}/lmp_serial
+
+${lmp} -in sph_multiphase_flow_precipitation_2d.lmp -var dname ${dname}
+
