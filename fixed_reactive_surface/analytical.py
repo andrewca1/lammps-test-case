03fe43cb4d796ebb0c36d637764955fb9f3f66a4
Quang T. Ha <qth20@bu.edu>
2018-01-08 16:15:07 -0500

    Adding some code for analytical plotting

--- fixed_reactive_surface/analytical.py
+++ fixed_reactive_surface/analytical.py
@@ -4,13 +4,12 @@ from scipy.optimize import brentq, fsolve
 import matplotlib.pyplot as plt
 
 # Parameters
-r1, r2 = 3, 15
+r1, r2 = 7, 30
 Ceq, C0 = 0.0, 1.0
 D = 1
 d = 0.5
 k = 1.14
 Da = k*r2/D
-t = 0.0
 
 # Bessel function of the first kind
 def J(n, x):
@@ -35,7 +34,7 @@ f = lambda x : (x*Y(1, x*r1/r2) + Da*Y(0, x*r1/r2))*J(0, x) \
     - (x*J(1, x*r1/r2) + Da*J(0, x*r1/r2))*Y(0, x)
 
 # Get a number of results
-X0 = np.linspace(0.1, 1.0, 100)
+X0 = np.arange(3.0, 60.0, 4.0)
 A = fsolve(f, X0)
 # Filtered out some duplicate elements
 A = list(set(A))
@@ -52,16 +51,39 @@ for i in range(len(A)):
 
 # Get the final roots
 alphas = [x for x in A if x not in delA]
-alphas = [alphas[0]]
 
 # Now we can obtain the concentration
 xr = np.linspace(r1, r2, 10000)
-C = xr*0.0
-for alpha in alphas:
-    C += np.pi*np.exp(-(alpha**2)*t*k/(r2*Da))*(Da**2)*(J(0, alpha)**2)*B(xr, alpha)*(1 - Ceq/C0)/F(alpha)
+times = [235, 270]
 
-C = 1.0 + (r1/r2)*Da*(1 - Ceq/C0)*np.log(xr/r2)/(1 + (r1/r2)*Da*np.log(r2/r1)) - C
+for t in times:
+    C = xr*0.0
+    for alpha in alphas:
+        C += np.pi*np.exp(-(alpha**2)*t*k/(r2*Da))*(Da**2)*(J(0, alpha)**2)*B(xr, alpha)*(1 - Ceq/C0)/F(alpha)
 
-# Plot the solution
-plt.plot(xr, C)
+    C = 1.0 + (r1/r2)*Da*(1 - Ceq/C0)*np.log(xr/r2)/(1 + (r1/r2)*Da*np.log(r2/r1)) - C
+    
+    # Plot the solution
+    plt.plot(xr, C, label="t={}".format(t))
+
+h = 0.015
+times = [10, 50]
+    
+for time in times:
+    # read data from output
+    data = np.loadtxt("dump_{}.last.xs".format(time), skiprows=9)
+
+    # Get only at the boundary
+    N = len(data[:,2])
+    x, y = [], []
+    for i in range(N):
+        if (0.5 >= data[i,2]) and (0.1 <= data[i,2]):
+            x.append(data[i, 2]/h)
+            y.append(data[i, 5])
+
+    # Revert the x
+    x = x[::-1]
+    plt.scatter(x, y, linestyle='-', s=5, label="sph-{}".format(time))
+    
+plt.legend(loc=4)
 plt.show()
--- fixed_reactive_surface/plot_graph.py
+++ fixed_reactive_surface/plot_graph.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import itertools
 
 # Time series
-T = [10]
+T = [10, 50]
+
+# Size
+h = 0.015
 
 # plot
 plt.figure(figsize=(16, 9))
@@ -18,12 +21,23 @@ for time in T:
     # read data from output
     data = np.loadtxt("dump_{}.last.xs".format(time), skiprows=9)
 
+    # Get only at the boundary
+    N = len(data[:,2])
+    x, y = [], []
+    for i in range(N):
+        if (0.5 >= data[i,2]) and (0.1 <= data[i,2]):
+            x.append(data[i, 2]/h)
+            y.append(data[i, 5])
+
+    # Revert the x
+    x = x[::-1]
+
     color = next(ax._get_lines.prop_cycler)['color']
-    plt.plot(data[:,2], data[:,5], linestyle='', markeredgecolor='none', marker=next(marker), color=color, label="sph-{}".format(time))
+    plt.plot(x, y, linestyle='', markeredgecolor='none', marker=next(marker), color=color, label="sph-{}".format(time))
     
 plt.ylabel("Concentration")
-plt.xlim(np.min(data[:,2]), np.max(data[:,2]))
-plt.ylim(np.min(data[:,5]), np.max(data[:,5]))
+plt.xlim(np.min(x), np.max(x))
+plt.ylim(np.min(y), np.max(y))
 plt.xlabel("x")
 plt.legend()
 plt.savefig("results.png")
