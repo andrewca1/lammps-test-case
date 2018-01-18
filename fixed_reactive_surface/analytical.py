646f642b6402eee25e8a462d37cc39aa9b95ee1d
Quang T. Ha <qth20@bu.edu>
2018-01-18 15:17:15 -0500

    Somewhat working analytical solution...

--- fixed_reactive_surface/analytical.py
+++ fixed_reactive_surface/analytical.py
@@ -3,15 +3,6 @@ import numpy as np
 from scipy.optimize import brentq, fsolve
 import matplotlib.pyplot as plt
 
-# Parameters
-h = 0.03125
-r1, r2 = 3.0*h, 15.0*h
-Ceq, C0 = 0.0, 1.0
-D = 1.0e-3
-R = 8.0e-3
-k = R*1.75/h
-Da = k*r2/D
-
 # Bessel function of the first kind
 def J(n, x):
     return sp.jn(n, x)
@@ -34,57 +25,74 @@ def B(r, alpha):
 f = lambda x : (x*Y(1, x*r1/r2) + Da*Y(0, x*r1/r2))*J(0, x) \
     - (x*J(1, x*r1/r2) + Da*J(0, x*r1/r2))*Y(0, x)
 
+# Get analytical solution
+def analytical(_X0, _t, _k, _Da, _C0, _Ceq, _r1, _r2, _nval):
+    _A = fsolve(f, _X0)
+    # Filtered out some duplicate elements
+    _A = list(set(_A))
+    # Sort
+    _A.sort()
+    
+    # Remove one more elements
+    _tol = 1.0e-6
+    _delA = []
+    _temp = _A[0]
+    for _i in range(len(_A)):
+        if (abs(_A[_i]-_A[_i-1]) <= _tol):
+            _delA.append(_A[_i])
+            
+    # Get the final roots
+    _alphas = [_x for _x in _A if _x not in _delA]
+    
+    # Now we can obtain the concentration
+    _xr = np.linspace(_r1, _r2, _nval)
+
+    # Summing the transient term
+    _C = _xr*0.0
+    for _alpha in _alphas:
+        _C -= np.pi*np.exp(-(_alpha**2)*_t*_k/(_r2*_Da))*(_Da**2)*(J(0, _alpha)**2)*B(_xr, _alpha)*(1 - _Ceq/_C0)/F(_alpha)
+
+    # Adding the solution
+    _C += 1.0 + (_r1/_r2)*_Da*(1 - _Ceq/_C0)*np.log(_xr/_r2)/(1 + (_r1/_r2)*_Da*np.log(_r2/_r1))
+    _C = _C*_C0
+
+    return _xr, _C, _alphas
+
+# Parameters
+h = 1.0
+r1, r2 = 3.0*h, 15.0*h
+Ceq, C0 = 0.0, 1.0
+D = 1.0
+R = 8.0e-3
+
 # Get a number of results
-X0 = np.arange(1.0, 60.0, 5.0)
-A = fsolve(f, X0)
-# Filtered out some duplicate elements
-A = list(set(A))
-# Sort
-A.sort()
+X0 = np.arange(1.0, 60.0, 4.0)
 
-# Remove one more elements
-tol = 1.0e-6
-delA = []
-temp = A[0]
-for i in range(len(A)):
-    if (abs(A[i]-A[i-1]) <= tol):
-        delA.append(A[i])
+# Plot the analytical solution
+# TODO: Why the constant factor?
+k = R*0.89
+Da = k*r2/D 
+xr, C, alphas = analytical(X0, 50, k, Da, C0, Ceq, r1, r2, 10000)
+plt.plot(xr/h, C, label="t=50")
 
-# Get the final roots
-alphas = [x for x in A if x not in delA]
-
-# Now we can obtain the concentration
-xr = np.linspace(r1, r2, 10000)
-times = [20, 50, 1000]
-
-for t in times:
-    C = xr*0.0
-    for alpha in alphas:
-        C -= np.pi*np.exp(-(alpha**2)*t*k/(r2*Da))*(Da**2)*(J(0, alpha)**2)*B(xr, alpha)*(1 - Ceq/C0)/F(alpha)
-
-    C += 1.0 + (r1/r2)*Da*(1 - Ceq/C0)*np.log(xr/r2)/(1 + (r1/r2)*Da*np.log(r2/r1))
+# Size
+h = 0.03125
+r1 = 3.0*h
+times = [50]
+for time in times:
+    # read data from output
+    data = np.loadtxt("dump_{}.last.xs".format(time), skiprows=9)
+    dx = data[0,2]
     
-    # Plot the solution
-    plt.plot(xr, C, label="t={}".format(t))
+    # Get only at the boundary
+    N = len(data[:,2])
+    x, y = [], []
+    for i in range(N):
+        if (data[i,2] >= (0.5+r1)) and (abs(data[i,3]-0.5) <= (dx)):
+            x.append((data[i, 2]-0.5)/h)
+            y.append(data[i, 5])
 
-# # Size
-# h = 0.03125
-# r1 = 3.0*h
-# times = [20]
-# for time in times:
-#     # read data from output
-#     data = np.loadtxt("dump_{}.last.xs".format(time), skiprows=9)
-#     dx = data[0,2]
-    
-#     # Get only at the boundary
-#     N = len(data[:,2])
-#     x, y = [], []
-#     for i in range(N):
-#         if (data[i,2] >= (0.5+r1)) and (abs(data[i,3]-0.5) <= (dx)):
-#             x.append((data[i, 2]-0.5))
-#             y.append(data[i, 5])
-
-#     plt.scatter(x, y, s=10, label="sph-{}".format(time))
+    plt.scatter(x, y, s=10, label="sph-{}".format(time))
     
 plt.legend(loc=4)
 plt.show()
