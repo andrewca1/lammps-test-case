8828eb84eb8edb8392f568f1d2a00f934a453020
Quang Ha <qth20@bu.edu>
2018-01-25 02:06:52 -0500

    Change to mpi code - seems to work

--- multiphase_concentration_diffusion/run.sh
+++ multiphase_concentration_diffusion/run.sh
@@ -3,11 +3,11 @@
 # Change directory to exe
 exedir=/home/qth20/Documents/lammps-sph/lammps/src
 
-# lmp=${exedir}/lmp_mpi
+lmp=${exedir}/lmp_mpi
 
-# mpirun --mca orte_base_help_aggregate 0 -np 4 ${lmp} -in sph_conc_diffusion_2d.lmp 
+mpirun --mca orte_base_help_aggregate 0 -np 4 ${lmp} -in sph_multiphase_conc_diffusion_2d.lmp 
 
-lmp=${exedir}/lmp_serial
+# lmp=${exedir}/lmp_serial
 
-${lmp} -in sph_multiphase_conc_diffusion_2d.lmp 
+# ${lmp} -in sph_multiphase_conc_diffusion_2d.lmp 
 
