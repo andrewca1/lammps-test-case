42ee1532562e990533ecb6edbf953dad720bdd91
Quang Ha <qth20@bu.edu>
2018-02-03 19:02:44 -0500

    WOrking flow + reactive surface

--- flow_fixed_reactive_surface/run.sh
+++ flow_fixed_reactive_surface/run.sh
@@ -3,9 +3,12 @@
 # Change directory to exe
 exedir=/home/qth20/Documents/lammps-sph/lammps/src
 
+dname=data-wall
+mkdir -p ${dname}
+
 lmp=${exedir}/lmp_mpi
 
-mpirun --mca orte_base_help_aggregate 0 -np 4 ${lmp} -in sph_flow_fixed_reactive_surface_2d.lmp 
+mpirun --mca orte_base_help_aggregate 0 -np 8 ${lmp} -in sph_flow_fixed_reactive_surface_2d.lmp -var dname ${dname}
 
 # lmp=${exedir}/lmp_serial
 
