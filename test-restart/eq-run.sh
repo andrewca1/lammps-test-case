cad5120907accf0cc2ce305e4e0f4971445994c8
Quang T. Ha <qth20@bu.edu>
2018-02-08 16:41:10 -0500

    Test restart status

---
+++ test-restart/eq-run.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Change directory to exe
+exedir=/home/qth20/Documents/lammps-sph/lammps/src
+
+dname=equil-wall
+mkdir -p ${dname}
+
+lmp=${exedir}/lmp_mpi
+
+mpirun --mca orte_base_help_aggregate 0 -np 8 ${lmp} -in equil_sph_flow_fixed_reactive_surface_2d.lmp -var dname ${dname}
+
+# lmp=${exedir}/lmp_serial
+
+# ${lmp} -in sph_fixed_reactive_surface_2d.lmp 
+
