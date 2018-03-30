f4018778b0181752c5251da632684c4d5d7f138c
Quang Ha <qth20@bu.edu>
2018-03-30 04:45:40 -0400

    Some working parameters

---
+++ flow_porous_reactive_surface/run.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Change directory to exe
+exedir=/home/qth20/Documents/lammps-sph/lammps/src
+
+dname=data-wall
+mkdir -p ${dname}
+
+lmp=${exedir}/lmp_mpi
+
+mpirun --mca orte_base_help_aggregate 0 -np 8 ${lmp} -in sph_flow_porous_reactive_surface_2d.lmp -var dname ${dname}
+
+# lmp=${exedir}/lmp_serial
+
+# ${lmp} -in sph_fixed_reactive_surface_2d.lmp 
+
