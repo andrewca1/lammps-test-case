6da24d8516f05f51247669ddf2549d3e3033854d
Quang T. Ha <qth20@bu.edu>
2018-04-16 18:01:46 -0400

    Workiing back on restarting

--- test-restart/eq-run.sh
+++ test-restart/eq-run.sh
@@ -3,12 +3,12 @@
 # Change directory to exe
 exedir=/home/qth20/Documents/lammps-sph/lammps/src
 
-dname=equil-wall
+dname=eq-data-wall
 mkdir -p ${dname}
 
 lmp=${exedir}/lmp_mpi
 
-mpirun --mca orte_base_help_aggregate 0 -np 8 ${lmp} -in equil_sph_flow_fixed_reactive_surface_2d.lmp -var dname ${dname}
+mpirun --mca orte_base_help_aggregate 0 -np 8 ${lmp} -in eq_sph_inlet_flow_fixed_reactive_surface_2d.lmp -var dname ${dname}
 
 # lmp=${exedir}/lmp_serial
 
