a7f3a4b2027387d84756bc915cb48ab614ffeffc
Quang Ha <qth20@bu.edu>
2018-05-17 19:18:35 -0400

    Add working wall

---
+++ multiphase_precipitation_wall/run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Change directory to exe
+exedir=/home/qth20/Documents/lammps-sph/lammps/src
+
+# lmp=${exedir}/lmp_mpi
+
+# mpirun --mca orte_base_help_aggregate 0 -np 2 ${lmp} -in sph_multiphase_precipitation_2d.lmp 
+
+lmp=${exedir}/lmp_serial
+
+${lmp} -in sph_multiphase_precipitation_2d.lmp 
